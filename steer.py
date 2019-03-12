d7bd5128c1ed09602432194e94a9e6a62fff4f23
Jetson <nguyenvietanh12597@gmail.com>
2019-03-12 12:43:59 +0700

    push jetson code

---
+++ steer.py
@@ -0,0 +1,182 @@
+import cv2
+import numpy as np
+import time
+import math
+from collections import deque
+total_time = 0
+last = time.time()
+pre_flag = 0
+map = []
+total_distance = 0.0
+last_speed = 0.0
+first_tine = True
+stored_time = 0.0
+
+
+class SegmentToSteer():
+    def __init__(self, square=3, margin=30, roi=1/3, size=10):
+        self.square = square
+        self.margin = margin
+        self.size = size
+        self.memory = deque(iterable=np.zeros(size, dtype=np.uint8), maxlen=size)
+        self.roi = 1 - roi
+        self.speed_max = 15
+        self.speed_min = 10
+        # self.steer_pid = PID(Kp=self.p[0], Ki=self.p[1], Kd=self.p[2])
+        # self.steer_pid.setWindup(50)
+        self.error_proportional_ = 0.0
+        self.error_integral_     = 0.0
+        self.error_derivative_   = 0.0
+        self.k_p = 0.5
+        self.k_i = 0.00
+        self.k_d = 0.00
+        self.inc_p = 0.1
+        self.inc_i = 0.1
+        self.inc_d = 0.1
+
+    def pid(self, cte):
+        self.error_integral_     += cte
+        self.error_derivative_    = cte - self.error_proportional_
+        self.error_proportional_  = cte
+
+        return -(self.k_p * self.error_proportional_ + self.k_i * self.error_integral_ + self.k_d * self.error_derivative_)
+
+    def get_point(self, img, flag):
+        IMG_H, IMG_W = img.shape
+        i = int(IMG_H * self.roi)
+        border = int((self.square - 1) / 2)
+        i_l = border
+        i_r = IMG_W - 1 - border
+        turn_left = False
+        turn_right = False
+        while i_l < IMG_W - border:
+            check = img[i - border: i + border + 1, i_l - border: i_l + border + 1]
+            white = np.sum(check) / 255
+            if white == self.square ** 2:
+                if i_l <= self.margin:
+                    turn_left = True
+                break
+            i_l += (border + 1)
+        while i_r > i_l:
+            check = img[i - border: i + border + 1, i_r - border: i_r + border + 1]
+            white = np.sum(check) / 255
+            if white == self.square ** 2:
+                if i_r >= IMG_W - self.margin:
+                    turn_right = True
+                break
+            i_r -= (border + 1)
+        if (turn_left and turn_right and flag == 1) or (turn_right and not turn_left):
+            while img[i][i_r] == 255 and i >= 0:
+                i -= 1
+            return i+1, i_r
+        elif (turn_left and turn_right and flag == -1) or (turn_left and not turn_right):
+            while img[i][i_l] == 255 and i >= 0:
+                i -= 1
+            return i+1, i_l
+        else:
+            return i, int((i_l + i_r) / 2)
+
+    def get_base_point(self, img):
+        IMG_H, IMG_W = img.shape
+        border = int((self.square - 1) / 2)
+        i = IMG_H - 1 - border
+        i_l = border
+        i_r = IMG_W - 1 - border
+        turn_left = False
+        turn_right = False
+        while i_l < IMG_W - border:
+            check = img[i - border: i + border + 1, i_l - border: i_l + border + 1]
+            white = np.sum(check) / 255
+            if white == self.square ** 2:
+                if i_l <= self.margin:
+                    turn_left = True
+                break
+            i_l += (border + 1)
+        while i_r > i_l:
+            check = img[i - border: i + border + 1, i_r - border: i_r + border + 1]
+            white = np.sum(check) / 255
+            if white == self.square ** 2:
+                if i_r >= IMG_W - self.margin:
+                    turn_right = True
+                break
+            i_r -= (border + 1)
+        return i, int((i_l + i_r) / 2)
+
+    def get_flag(self):
+        arr = np.asarray(self.memory, np.int8)
+        return np.bincount(arr).argmax() - 1
+
+    def write_image(self, raw, label, x, y, steer):
+        raw[label == 255] = [128, 64, 128]
+        font = cv2.FONT_HERSHEY_DUPLEX
+        bottomLeftCornerOfText = (180, 158)
+        fontScale = 0.5
+        fontColor = (0, 255, 0)
+        lineType = 1
+        cv2.putText(raw, str(steer.__format__(".0f")), bottomLeftCornerOfText, font, fontScale, fontColor, lineType, 0)
+        cv2.line(raw, (x, y), (160, 160), fontColor, thickness=2)
+        raw = cv2.resize(raw, (480, 240))
+        return raw
+
+    def write_vector(self, img, x, y, steer):
+        IMG_H, IMG_W = img.shape
+        font = cv2.FONT_HERSHEY_DUPLEX
+        bottomLeftCornerOfText = (180, 158)
+        fontScale = 0.5
+        fontColor = (0, 255, 0)
+        lineType = 1
+        cv2.putText(img, str(steer.__format__(".0f")), bottomLeftCornerOfText, font, fontScale, fontColor, lineType, 0)
+        cv2.line(img, (int (IMG_H / 2), IMG_W), (x, y), (0, 0, 0), 1)
+        return img
+
+    def get_steer(self, raw, label, flag, s):
+        global pre_flag
+        global total_time
+        global last
+        global first_tine
+        global stored_time
+        global last_speed
+        IMG_H, IMG_W = label.shape
+        interval = time.time() - last
+        last = time.time()
+        current_flag = 0
+        roi = self.roi
+
+        if total_time > 0 and total_time < 3:
+            total_time += interval
+            current_flag = pre_flag
+        else:
+            total_time = 0
+            self.memory.append(flag+1)
+            current_flag = self.get_flag()
+            if current_flag != 0 and current_flag != pre_flag:
+                pre_flag = current_flag
+                total_time += interval
+            else:
+                pre_flag = current_flag
+
+        y, x = self.get_point(label, current_flag)
+
+        while label[y][x] == 0 and roi < 0.8:
+            roi += 0.1
+            y, x = self.get_point(label, current_flag)
+
+        steer = np.arctan((x - IMG_W/2 + 1) / (IMG_H - float(y))) * 57.32
+        steer = np.sign(steer) * min(50, abs(steer))
+        #PID tuning
+        #self.steer_pid.updateError(steer)
+        #new_steer = -self.steer_pid.output()
+        new_steer = -self.pid(x - IMG_W/2 + 1 )
+        # print steer, new_steer
+        # print self.steer_pid.Kp, self.steer_pid.Ki, self.steer_pid.Kd
+        # twiddle_count_threshold = 500
+        # if self.steer_pid.update_count > twiddle_count_threshold:
+        #     self.steer_pid.twiddle()
+        if current_flag != 0 or pre_flag != 0:
+            speed = self.speed_min
+        else:
+            #speed = max(self.speed_min, self.speed_max*np.cos(abs(steer)*np.pi/180))
+            speed = self.speed_max
+            # speed = self.speed_max
+        # label = self.write_vector(label, x, y, steer)
+        return speed, new_steer, label
---
+++ team107_node.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import roslib
+roslib.load_manifest('team107')
+import sys
+import rospy
+import cv2
+from std_msgs.msg import String, Float32, Bool
+from sensor_msgs.msg import CompressedImage
+from cv_bridge import CvBridge, CvBridgeError
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import model_from_json, load_model
+#from signRecognition import detect1
+import time
+import rospkg
+from steer import SegmentToSteer
+
+rospack = rospkg.RosPack()
+path = rospack.get_path('team107') + '/scripts/'
+end = time.time()
+start = time.time()
+check = True
+is_running = True
+
+class Processor:
+	def __init__(self, model):
+		self.image = None
+		self.model = model
+		self.graph = tf.get_default_graph()
+		self.ss_sub = rospy.Subscriber('ss_status', Bool, self.run_callback, queue_size=1)
+		self.image_sub = rospy.Subscriber('/camera/rgb/image_raw/compressed', CompressedImage, self.callback, queue_size=1)
+		self.pub_speed = rospy.Publisher('/set_speed_car_api', Float32, queue_size=1)
+		self.pub_steerAngle = rospy.Publisher('/set_steer_car_api', Float32, queue_size=1)
+		self.lastTime = time.time()
+		self.s2s = SegmentToSteer(square=3, margin=10, roi=0.3)
+
+	def run_callback(self, data):
+		global is_running
+		is_running = data.data
+
+	def callback(self, data):
+		global end
+		global is_running
+		global check
+		global start
+		# if check == True:
+		# 	start = time.time()
+		# 	check = False
+		delta = time.time() - end
+		if delta >= 0.03 and is_running == True:
+			try:
+				with self.graph.as_default():
+					self.image = self.convert_data_to_image(data.data)
+					#flag, s = detect1(self.image)
+					res = self.get_segment_image(self.image)
+					#cv2.imshow('black and white', res*255.)
+					#cv2.waitKey(1)
+					speed, steer, res = self.s2s.get_steer(self.image, res*255., 0, 0)
+					cv2.imshow('road', res)
+					cv2.waitKey(1)
+					# if time.time() - start <= 10:
+					# 	speed = 100
+					self.publish_data(speed, -2/3*steer)
+
+			except CvBridgeError as e:
+				print(e)
+			end = time.time()
+		elif is_running == False:
+			self.s2s.error_proportional_ = 0.0
+			self.s2s.error_integral_     = 0.0
+			self.s2s.error_derivative_   = 0.0
+			self.publish_data(0, -10)
+
+	def convert_data_to_image(self, data):
+		arr = np.fromstring(data, np.uint8)
+		image = cv2.imdecode(arr, 1)
+		image = cv2.resize(image, (320,160))
+		return image
+
+	def get_segment_image(self, image):
+		res = self.model.predict(np.expand_dims(image/255., axis=0))
+		return np.argmax(res, axis=3)[0]
+
+	def publish_data(self, speed, steerAngle):
+		self.pub_speed.publish(speed)
+		self.pub_steerAngle.publish(steerAngle)
+
+def main(args):
+	p = processor()
+	rospy.init_node('team107')
+	try:
+		rospy.spin()
+	except:
+		print("Shutting down")
+
+if __name__ == '__main__':
+	main(sys.argv)
