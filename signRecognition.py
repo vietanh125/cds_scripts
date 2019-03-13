a73f1f4767a0c049b32bcbaeeb37d360fa5a6b73
Jetson <nguyenvietanh12597@gmail.com>
2019-03-13 23:53:34 +0700

    fix signRecognition

--- signRecognition.py
+++ signRecognition.py
@@ -3,7 +3,7 @@ from imutils import contours
 import imutils
 import cv2
 import numpy as np
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 import rospkg
 rospack = rospkg.RosPack()
 path = rospack.get_path('team107') + '/scripts/'
