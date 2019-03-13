4d108defe19912c6f308361af6c9d6092f6215f5
vietanh125 <33384701+vietanh125@users.noreply.github.com>
2019-03-13 14:35:31 +0700

    Update util.py

--- util.py
+++ util.py
@@ -16,15 +16,13 @@ from team107_node import Processor
 from sensor_msgs.msg import CompressedImage
 from std_msgs.msg import String, Float32, Bool
 from rospy import ROSException
-from tensorflow.keras.layers import *
-from tensorflow.keras.models import Model
-from tensorflow.keras import applications
 
 rospack = RosPack()
 
+
 class Utilities:
     def __init__(self):
-        #params
+        # params
         self.count = 1
         self.clear_str = '0:0:                                                                                '
         self.is_recording = False
@@ -37,23 +35,23 @@ class Utilities:
         self.bt2_status = False
         self.bt3_status = False
         self.bt4_status = False
-	self.ss_status = False
+        self.ss_status = False
         self.model = self.load_model()
-        #ros subscribers and publishers
-        #hal
+        # ros subscribers and publishers
+        # hal
         self.sub_bt1 = rospy.Subscriber('/bt1_status', Bool, self.bt1_callback, queue_size=1)
         self.sub_bt2 = rospy.Subscriber('/bt2_status', Bool, self.bt2_callback, queue_size=1)
         self.sub_bt3 = rospy.Subscriber('/bt3_status', Bool, self.bt3_callback, queue_size=1)
         self.sub_bt4 = rospy.Subscriber('/bt4_status', Bool, self.bt4_callback, queue_size=1)
         self.sub_ss = rospy.Subscriber('/ss_status', Bool, self.ss_callback, queue_size=1)
         self.pub_lcd = rospy.Publisher('/lcd_print', String, queue_size='5')
-        #car controller
+        # car controller
         self.sub_img = None
-    	rospy.init_node('team107')
+        rospy.init_node('team107')
         print 'Finish initializing'
-        #finish
+        # finish
 
-    #setup callbacks
+    # setup callbacks
     def bt1_callback(self, data):
         self.bt1_status = data.data
 
@@ -76,38 +74,40 @@ class Utilities:
         cv2.imshow('image', image)
         cv2.waitKey(1)
 
-    #button interactions
+    # button interactions
     def check_btns(self):
         if not self.is_recording and not self.is_self_driving:
-			try:
-				if self.first_load:
-					self.pub_lcd.publish(self.clear_str)
-					self.pub_lcd.publish('0:0:welcome Fast n Fiery')
-					self.first_load = False
-			except ROSException:
-				pass
-			if self.bt1_status and not self.bt2_status and not self.bt3_status and not self.bt4_status:
-				self.setup_record()
-				self.record_control()
-			elif not self.bt1_status and self.bt2_status and not self.bt3_status and not self.bt4_status:
-				self.setup_engine()
-				self.engine_control()
+            try:
+                if self.first_load:
+                    self.pub_lcd.publish(self.clear_str)
+                    self.pub_lcd.publish('0:0:welcome Fast n Fiery')
+                    self.first_load = False
+            except ROSException:
+                pass
+            if self.bt1_status and not self.bt2_status and not self.bt3_status and not self.bt4_status:
+                self.setup_record()
+                self.record_control()
+            elif not self.bt1_status and self.bt2_status and not self.bt3_status and not self.bt4_status:
+                self.setup_engine()
+                self.engine_control()
         elif self.is_recording:
             self.record_control()
         elif self.is_self_driving:
             self.engine_control()
 
-    #record functions
+    # record functions
     def setup_record(self):
         self.is_recording = True
         self.is_self_driving = False
         self.bt1_status = False
         fourcc = cv2.VideoWriter_fourcc(*'XVID')
-        self.video_recorder = cv2.VideoWriter(self.path + '/segment_data/' + str(time.time()) + '.mp4', fourcc, 20.0, (640, 480))
+        self.video_recorder = cv2.VideoWriter(self.path + '/segment_data/' + str(time.time()) + '.mp4', fourcc, 20.0,
+                                              (640, 480))
         self.pub_lcd.publish(self.clear_str)
-        self.sub_img = rospy.Subscriber('/camera/rgb/image_raw/compressed', CompressedImage, self.image_callback, queue_size=1)
+        self.sub_img = rospy.Subscriber('/camera/rgb/image_raw/compressed', CompressedImage, self.image_callback,
+                                        queue_size=1)
         self.pub_lcd.publish('0:0:' + self.free_space('/home'))
-    	print 'start recording'
+        print 'start recording'
         time.sleep(1)
 
     def record_control(self):
@@ -124,7 +124,7 @@ class Utilities:
         print 'turnning off'
         time.sleep(1)
 
-    #engine functions
+    # engine functions
     def setup_engine(self):
         self.is_self_driving = True
         self.is_recording = False
@@ -145,7 +145,7 @@ class Utilities:
             self.change_max_speed(-1)
         elif not self.bt1_status and self.bt2_status and not self.bt3_status and self.bt4_status:
             self.change_min_speed(-1)
-        elif not self.bt1_status and not self.bt2_status and self.bt3_status and self.bt4_status:        
+        elif not self.bt1_status and not self.bt2_status and self.bt3_status and self.bt4_status:
             self.change_roi(-0.1)
 
     def change_max_speed(self, amount):
@@ -159,10 +159,10 @@ class Utilities:
         time.sleep(0.5)
 
     def change_roi(self, amount):
-		change = self.engine.s2s.roi + amount
-		self.engine.s2s.roi = max(0.1, min(change, 0.9))
-		self.print_car_stats(self.engine.s2s.speed_max, self.engine.s2s.speed_min, self.engine.s2s.roi)
-		time.sleep(0.5)
+        change = self.engine.s2s.roi + amount
+        self.engine.s2s.roi = max(0.1, min(change, 0.9))
+        self.print_car_stats(self.engine.s2s.speed_max, self.engine.s2s.speed_min, self.engine.s2s.roi)
+        time.sleep(0.5)
 
     def turn_off_engine(self):
         self.is_self_driving = False
@@ -173,43 +173,48 @@ class Utilities:
         self.first_time = True
         time.sleep(1)
 
-    #utility functions
-    def load_model(self):
-		mbl = applications.mobilenet.MobileNet(weights=None, include_top=False, input_shape=(160, 320, 3))
-		x = mbl.output
-		model_tmp = Model(inputs=mbl.input, outputs=x)
-		layer5, layer8, layer13 = model_tmp.get_layer('conv_pw_5_relu').output, model_tmp.get_layer(
-			'conv_pw_8_relu').output, model_tmp.get_layer('conv_pw_13_relu').output
-		fcn14 = Conv2D(filters=2, kernel_size=1, name='fcn14')(layer8)
-		fcn16 = Conv2DTranspose(filters=layer5.get_shape().as_list()[-1], kernel_size=4, strides=2, padding='same',
-								name="fcn16_conv2d")(fcn14)
-		# Add skip connection
-		fcn16_skip_connected = Add(name="fcn16_plus_vgg_layer5")([fcn16, layer5])
-		# Upsample again
-		fcn17 = Conv2DTranspose(filters=2, kernel_size=16,
-								strides=(8, 8), padding='same', name="fcn17", activation="softmax")(fcn16_skip_connected)
-		model = Model(inputs=mbl.input, outputs=fcn17)
-		model.load_weights(self.path + "model-mobilenet-1M-iter12-pretrain-bdd.h5")
-		return model
-    def load_model_segment(self):
-		mbl = applications.mobilenet.MobileNet(weights=None, include_top=False, input_shape=(160,320,3))
-		x = mbl.output
-		model_tmp =  Model(inputs = mbl.input, outputs = x)
-		layer5, layer8, layer13 = model_tmp.get_layer('conv_pw_5_relu').output, model_tmp.get_layer('conv_pw_8_relu').output, model_tmp.get_layer('conv_pw_13_relu').output
+    # utility functions
+    def load_model():
+        mbl = applications.mobilenet.MobileNet(weights=None, include_top=False, input_shape=(160, 320, 3))
+        x = mbl.output
+        model_tmp = Model(inputs=mbl.input, outputs=x)
+        layer5, layer8, layer13 = model_tmp.get_layer('conv_pw_5_relu').output, model_tmp.get_layer(
+            'conv_pw_8_relu').output, model_tmp.get_layer('conv_pw_13_relu').output
+        fcn14 = Conv2D(filters=2, kernel_size=1, name='fcn14')(layer8)
+        fcn16 = Conv2DTranspose(filters=layer5.get_shape().as_list()[-1], kernel_size=4, strides=2, padding='same',
+                                name="fcn16_conv2d")(fcn14)
+        # Add skip connection
+        fcn16_skip_connected = Add(name="fcn16_plus_vgg_layer5")([fcn16, layer5])
+        # Upsample again
+        fcn17 = Conv2DTranspose(filters=2, kernel_size=16, strides=(8, 8), padding='same', name="fcn17",
+                                activation="softmax")(fcn16_skip_connected)
+        model = Model(inputs=mbl.input, outputs=fcn17)
+        model.load_weights('model_1M.h5')
+        return model
 
-		fcn14 = Conv2D(filters=2 , kernel_size=1, name='fcn14')(layer13)
-		fcn15 = Conv2DTranspose(filters=layer8.get_shape().as_list()[-1] , kernel_size=4, strides=2, padding='same', name='fcn15')(fcn14)
-		fcn15_skip_connected = Add(name="fcn15_plus_vgg_layer8")([fcn15, layer8])
-		fcn16 = Conv2DTranspose(filters=layer5.get_shape().as_list()[-1], kernel_size=4, strides=2, padding='same', name="fcn16_conv2d")(fcn15_skip_connected)
-		# Add skip connection
-		fcn16_skip_connected = Add(name="fcn16_plus_vgg_layer5")([fcn16, layer5])
-		# Upsample again
-		fcn17 = Conv2DTranspose(filters=2, kernel_size=16, strides=(8, 8), padding='same', name="fcn17", activation="softmax")(fcn16_skip_connected)
-		m = Model(inputs = mbl.input, outputs = fcn17)
-		m.load_weights(self.path + 'model-mobilenet-iter2-pretrain-data-bdd.h5')
-		m.predict(np.zeros((1, 160, 320, 3), dtype=np.float32))
-		print("Model loaded")
-		return m
+    def load_model_segment(self):
+        mbl = applications.mobilenet.MobileNet(weights=None, include_top=False, input_shape=(160, 320, 3))
+        x = mbl.output
+        model_tmp = Model(inputs=mbl.input, outputs=x)
+        layer5, layer8, layer13 = model_tmp.get_layer('conv_pw_5_relu').output, model_tmp.get_layer(
+            'conv_pw_8_relu').output, model_tmp.get_layer('conv_pw_13_relu').output
+
+        fcn14 = Conv2D(filters=2, kernel_size=1, name='fcn14')(layer13)
+        fcn15 = Conv2DTranspose(filters=layer8.get_shape().as_list()[-1], kernel_size=4, strides=2, padding='same',
+                                name='fcn15')(fcn14)
+        fcn15_skip_connected = Add(name="fcn15_plus_vgg_layer8")([fcn15, layer8])
+        fcn16 = Conv2DTranspose(filters=layer5.get_shape().as_list()[-1], kernel_size=4, strides=2, padding='same',
+                                name="fcn16_conv2d")(fcn15_skip_connected)
+        # Add skip connection
+        fcn16_skip_connected = Add(name="fcn16_plus_vgg_layer5")([fcn16, layer5])
+        # Upsample again
+        fcn17 = Conv2DTranspose(filters=2, kernel_size=16, strides=(8, 8), padding='same', name="fcn17",
+                                activation="softmax")(fcn16_skip_connected)
+        m = Model(inputs=mbl.input, outputs=fcn17)
+        m.load_weights(self.path + 'model-mobilenet-iter2-pretrain-data-bdd.h5')
+        m.predict(np.zeros((1, 160, 320, 3), dtype=np.float32))
+        print("Model loaded")
+        return m
 
     def convert_to_image(self, data):
         arr = np.fromstring(data, np.uint8)
@@ -220,7 +225,7 @@ class Utilities:
     def free_space(self, path):
         st = os.statvfs(path)
         free = st.f_bavail * st.f_frsize
-        return "free " + str(free/(1000*1000)) + "MB"
+        return "free " + str(free / (1000 * 1000)) + "MB"
 
     def print_car_stats(self, speed_max, speed_min, roi):
         self.pub_lcd.publish(self.clear_str)
@@ -228,14 +233,16 @@ class Utilities:
         self.pub_lcd.publish('7:0:min ' + str(speed_min))
         self.pub_lcd.publish('0:2:roi ' + str(roi))
 
+
 def main(args):
-	gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.5)
-	sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
-	util = Utilities()
-	try:
-		rospy.spin()
-	except:
-		print("Shutting down")
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.5)
+    sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+    util = Utilities()
+    try:
+        rospy.spin()
+    except:
+        print("Shutting down")
+
 
 if __name__ == '__main__':
-	main(sys.argv)
+    main(sys.argv)
